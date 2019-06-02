f9efceeba6bfada5e405042516bc6afbcb447213
anuj007jain <anuj007jain@gmail.com>
2019-06-02 13:48:34 -0700

    new interview problems

--- src/com/Algorithms_2_Coursera/Week1/DirectedGraphs/TopologicalSort.java
+++ src/com/Algorithms_2_Coursera/Week1/DirectedGraphs/TopologicalSort.java
@@ -1,5 +1,8 @@
 package com.Algorithms_2_Coursera.Week1.DirectedGraphs;
 
+import com.Algorithms_2_Coursera.Week2.ShortestPaths.DirectedWeightedEdge;
+import com.Algorithms_2_Coursera.Week2.ShortestPaths.DirectedWeightedGraph;
+
 import java.util.Stack;
 
 /**
@@ -9,7 +12,7 @@ import java.util.Stack;
 public class TopologicalSort {
 
     boolean[] marked;
-    Stack<Integer> stack = new Stack<>();
+    public Stack<Integer> stack = new Stack<>();
 
     public TopologicalSort(Digraph G) {
         marked = new boolean[G.V()];
@@ -27,8 +30,32 @@ public class TopologicalSort {
                 dfs(G, w);
             }
         }
-        System.out.println(v);
+        //System.out.println(v);
         stack.push(v); // the vertex has been completely visited and hence can be pushed on to stack
+    }
+
+    public TopologicalSort(DirectedWeightedGraph G) {
+        marked = new boolean[G.V()];
+        for (int v = 0 ; v < G.V() ; v++) {
+            if(!marked[v]) {
+                visit(G, v);
+            }
+        }
+    }
+
+    private void visit(DirectedWeightedGraph G, int v) {
+        marked[v] = true;
+        for (DirectedWeightedEdge e: G.adj(v)) {
+            int w = e.to();
+            if(!marked[w]) {
+                visit(G, w);
+            }
+        }
+        stack.push(v);
+    }
+
+    public Iterable<Integer> order() {
+        return stack;
     }
 
     public static void main(String[] args) {
@@ -44,5 +71,30 @@ public class TopologicalSort {
         G.addEdge(5,2);
         G.addEdge(6,0);
         TopologicalSort sort = new TopologicalSort(G);
+
+        for (int v : sort.order()) {
+            System.out.print(v+" ");
+        }
+
+        //weights don't matter for topological sorting
+        DirectedWeightedGraph G2 = new DirectedWeightedGraph(7);
+        G2.addEdge(new DirectedWeightedEdge(0, 1, 0));
+        G2.addEdge(new DirectedWeightedEdge(0, 2, 0));
+        G2.addEdge(new DirectedWeightedEdge(0, 5, 0));
+        G2.addEdge(new DirectedWeightedEdge(1, 4, 0));
+        G2.addEdge(new DirectedWeightedEdge(3, 2, 0));
+        G2.addEdge(new DirectedWeightedEdge(3, 4, 0));
+        G2.addEdge(new DirectedWeightedEdge(3, 5, 0));
+        G2.addEdge(new DirectedWeightedEdge(3, 6, 0));
+        G2.addEdge(new DirectedWeightedEdge(5, 2, 0));
+        G2.addEdge(new DirectedWeightedEdge(6, 0, 0));
+
+        System.out.println();
+
+        sort = new TopologicalSort(G2);
+
+        for (int v : sort.order()) {
+            System.out.print(v+" ");
+        }
     }
 }
